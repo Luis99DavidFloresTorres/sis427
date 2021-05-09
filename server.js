7f1a1cdbaa830c4289172cf9349d7e79a8cb5626
Luis David Flores Torres <luisdavid99florestorres@gmail.com>
2021-05-09 18:57:48 -0400

    deployment3

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-app.use(express.static(__dirname + '/dist/practica2'));
+app.use(express.static(__dirname + '/dist/herokufront'));
 app.get('/*', function(req,res) {
 res.sendFile(path.join(__dirname+
-'/dist/practica2/index.html'));});
+'/dist/herokufront/index.html'));});
 app.listen(process.env.PORT || 8080);
